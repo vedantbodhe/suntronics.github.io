d2aabe0983f481a293363935a1f1a2aa91f33403
vedantbodhe <vedantbodhe@gmail.com>
2023-09-03 00:25:13 +0530

    php edit

--- forms/contact.php
+++ forms/contact.php
@@ -1,49 +1,31 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+  require 'PHPMailer/PHPMailerAutoload.php';
 
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'info@siipl.co';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
+  $mail = new PHPMailer;
+
+  $mail->isSMTP();
+  $mail->Host = 'smtp.gmail.com'; // Gmail SMTP server
+  $mail->SMTPAuth = true;
+  $mail->Username = 'info@siipl.co'; // Your Gmail email address
+  $mail->Password = 'MYRA%221122'; // Your Gmail password or App Password
+  $mail->SMTPSecure = 'tls'; // Use 'tls' for TLS encryption or 'ssl' for SSL encryption
+  $mail->Port = 587; // Port for TLS (465 for SSL)
+
+  // Sender and Recipient
+  $mail->setFrom($_POST['email'], $_POST['name']);
+  $mail->addAddress($receiving_email_address);
+
+  // Email Content
+  $mail->isHTML(true);
+  $mail->Subject = $_POST['subject'];
+  $mail->Body = $_POST['message'];
+
+  if ($mail->send()) {
+    echo 'OK';
   } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+    echo 'Error: ' . $mail->ErrorInfo;
   }
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->smtp = array(
-    'host' => 'smtp.gmail.com', // Gmail SMTP server
-    'username' => 'info@siipl.co', // Your Gmail email address
-    'password' => 'MYRA%221122', // Your Gmail password or App Password
-    'port' => 587, // Port for TLS encryption
-    'encryption' => 'tls', // Use 'tls' for TLS encryption or 'ssl' for SSL encryption
-); 
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
 ?>
